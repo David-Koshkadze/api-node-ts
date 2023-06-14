11459966984935c79b7bc6cd5c1feef766473c6e
David-Koshkadze <rootUS3rr@pm.me>
2023-06-14 21:39:11 +0400

    user schema

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import { Express, Request, Response } from "express";
 import { createUserHandler } from "./controller/user.controller";
+import { createUserSchema } from "./schema/user.schema";
+import validateResource from "./middleware/validateResource";
 
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
   app.get("/david", (req: Request, res: Response) => res.send("Hello World"))
 
-  app.post("/api/users", createUserHandler)
+  app.post("/api/users", validateResource(createUserSchema), createUserHandler)
 }
 
 export default routes;
