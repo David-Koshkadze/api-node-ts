81340a1178bfbe3fb61564f1f353dac0658e06d9
David-Koshkadze <rootUS3rr@pm.me>
2023-06-20 09:38:12 +0400

    validate password in service

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,15 +1,38 @@
+import { omit } from "lodash";
 import User, { UserDocument } from "../models/user.model";
-import logger from '../utils/logger'
+import logger from "../utils/logger";
 
-type UserInput = Omit<UserDocument, "createdAt" | "updatedAt" | "comparePassword">
+type UserInput = Omit<
+  UserDocument,
+  "createdAt" | "updatedAt" | "comparePassword"
+>;
 
 export async function createUser(
   input: any // some ts errors and will fix soon
 ) {
   try {
-    return await User.create(input);
+    const user = await User.create(input);
+    return omit(user.toJSON(), "password");
   } catch (e: any) {
     logger.error(e);
     throw new Error(e);
   }
+}
+
+export async function validatePassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const user = await User.findOne({ email });
+
+  if (!user) return false;
+
+  const isValid = await user.comparePassword(password);
+
+  if (!isValid) return false;
+
+  return omit(user.toJSON(), "password");
 }
