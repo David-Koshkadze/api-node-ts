0c5f5127e55c4b3ed2f9061cb11cd4704670709d
David-Koshkadze <rootUS3rr@pm.me>
2023-06-20 09:17:59 +0400

    session controller and service

---
+++ src/controller/session.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+export async function createUserSessionHandler(req: Request, res: Response) {
+
+  // Validate user's password
+
+  // create session
+
+  // create access token
+
+  // create refresh token
+
+  // return access and refresh tokens
+
+}
