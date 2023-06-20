b54d483b820942edd3c9625a44d76ff9cc2d2dbe
David-Koshkadze <rootUS3rr@pm.me>
2023-06-20 12:27:48 +0400

    implement jwt

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
 import logger from "../utils/logger";
-import {omit} from 'lodash'
-
+import { omit } from "lodash";
 
 import { createUser } from "../service/user.service";
 import { CreateUserInput } from "../schema/user.schema";
 
-export async function createUserHandler(req: Request<{}, {}, CreateUserInput["body"]>, res: Response) {
+export async function createUserHandler(
+  req: Request<{}, {}, CreateUserInput["body"]>,
+  res: Response
+) {
   try {
     const user = await createUser(req.body);
     res.send(omit(user.toJSON(), "password"));
